7bd239d7747fb768359ff4bd426f4521b43427c2
feichen0522 <feichen2012@outlook.com>
2018-10-27 10:43:31 +0800

    cf commit

--- file/models.py
+++ file/models.py
@@ -5,6 +5,10 @@ from user.models import User
 class ResourceType(models.Model):
     name=models.CharField(max_length=20)
 
+# 技术领域表
+class TechnicalField(models.Model):
+    name=models.CharField(max_length=20)
+
 # 这是文件资源的信息表
 class Resource(models.Model):
     # 后面要加上评论id
@@ -20,6 +24,7 @@ class Resource(models.Model):
     title=models.CharField(null=True,max_length=80)
     describe=models.CharField(null=True,max_length=80)
     resourceTypeId=models.ForeignKey(to=ResourceType,to_field='id',on_delete=models.CASCADE,default=1)
+    technicalFieldId=models.ForeignKey(to=TechnicalField,to_field='id',on_delete=models.CASCADE,default=1)
     # 用户收藏表
 class Collect(models.Model):
     user=models.ForeignKey(to=User,to_field='id',on_delete=models.CASCADE,default=1)
